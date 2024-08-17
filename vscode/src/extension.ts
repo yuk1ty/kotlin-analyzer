2151f559d556d685a87e13f8d783ed255ed0f734
yuk1ty <yuk1ty@users.noreply.github.com>
2024-08-17 21:47:25 +0900

    Add vscode client

---
+++ vscode/src/extension.ts
@@ -0,0 +1,48 @@
+import * as path from "path";
+import { workspace, ExtensionContext } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js"),
+  );
+
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+    },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "kotlin" }],
+    synchronize: {
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  client = new LanguageClient(
+    "vscodeKotlinAnalyzerClient",
+    "Kotlin Analyzer VScode Client",
+    serverOptions,
+    clientOptions,
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
